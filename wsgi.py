bff79fb3b3188f25b758d9007f86b7ed8d31c794
jatinkalal171-coder <jatinkalal171@gmail.com>
2026-09-02 18:57:18 +0530

    Add files via upload

--- wsgi.py
+++ wsgi.py
@@ -1,6 +1,12 @@
+import os
+import sys
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+sys.path.insert(0, BASE_DIR)
+
 from backend.app import create_app
 
 app = create_app()
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     app.run()
